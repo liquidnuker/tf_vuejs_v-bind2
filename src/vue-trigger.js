5347f97dfda48a6ca2e71a18b7117fb4bdbf1a69
administrator <liquidnuker@users.noreply.github.com>
2017-03-18 08:59:17 -0800

    initial commit

---
+++ src/vue-trigger.js
@@ -0,0 +1,12 @@
+var styler = new Vue({
+  el: '#app',
+  data: {
+    app_style: {
+      backgroundColor: '#666',
+      fontSize: '13px'
+    },
+    override_style: {
+      backgroundColor: 'green'
+    }
+  }
+});
